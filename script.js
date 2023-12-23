1c8b3c2e64c86ad5325bc1bf669b862ecbcd0fdf
Gavin Fletcher <grf4432@g.rit.edu>
2023-12-23 16:27:43 -0500

    Finished Stage 3 design Stage 2 --> Stage3 animations Commented js code

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ const positionSkills = {
 
 document.addEventListener('DOMContentLoaded', function() {
     const continueButton = document.getElementById('continueButton'); //Stage 1 button
-    const inputBox = document.getElementById('inputbox'); // Inoput box for name of player
+    const inputBox = document.getElementById('nameBox'); // Inoput box for name of player
     const mainDiv = document.querySelector('.mainDiv'); // Stage 1 div
     const continueButton2 = document.getElementById("continueButton2"); // Stage 2 Button
     const positionButtons = document.querySelectorAll(".pickPosition"); //Position buttons
+    const continueButton3 = document.getElementById("continueButton3");
+    const skillGrid = document.querySelectorAll(".skillsInput");
     let userInputValue = ''; // Gets the name of the player for final result page
     let userPosition = ''; // Keeps the user position from the button clicked
 
@@ -37,8 +39,8 @@ document.addEventListener('DOMContentLoaded', function() {
           setTimeout(function() {
               inputBox.classList.remove("leave-right");
               continueButton.classList.remove("leave-down");
-              mainDiv.style.display = "none"
-              staggerButtons();
+              mainDiv.style.display = "none" // Hide the main div.
+              staggerButtons(positionButtons, 200); //Make the positons animate
           }, 800)
           document.getElementsByClassName("button-container")[0].style.opacity = 1;
           document.addEventListener('DOMContentLoaded', staggerButtons);
@@ -52,16 +54,15 @@ document.addEventListener('DOMContentLoaded', function() {
       } 
     });
 
-    function staggerButtons() {
-      positionButtons.forEach((button, index) => {
+    // Function for the position button animation (fades in one at a time)
+    function staggerButtons(elementToLoop, timeDelay) {
+      elementToLoop.forEach((button, index) => {
         setTimeout(() => {
-          button.style.opacity = '1';
-          button.style.transform = 'translateY(0)';
-        }, index * 200); // Change the delay time (in milliseconds) as needed
+          button.style.cssText = "opacity: 1; transform: translateY(0);";
+        }, index * timeDelay); // Change the delay time (in milliseconds) as needed
       });
         document.getElementById("posP").style.opacity = 1;
-        continueButton2.style.opacity = 1;
-        continueButton2.style.transform = "translateY(0)";
+        continueButton2.style.cssText = "opacity: 1; transform: translateY(0);";
     }
 
     continueButton2.addEventListener("click", function() {
@@ -73,9 +74,23 @@ document.addEventListener('DOMContentLoaded', function() {
             isSelected = true;
           } 
         });
-        if (isSelected) {
+        if (isSelected) { // Runs the code within the statement IF the user has clicked a button.
+
+          // Hide step 2 and show step 3
           console.log(userPosition + " is the selected position!")
+          document.getElementsByClassName("button-container")[0].style.display = "none"; // Removes the main div from viewing
+          document.getElementById("parent-container").style.display = "flex";
+          continueButton2.style.display = "none";
+          continueButton2.style.opacity = 0;
+          continueButton3.style.cssText = 'display: flex; opacity: 1;';
+          console.log(window.getComputedStyle(continueButton2).display);
+          staggerButtons(skillGrid, 200);
+          staggerButtons(document.querySelectorAll(".skillLabel"), 250);
+          // Get the skill array:
+          continueButton3.style.cssText = "opacity: 1; transform: translateY(0);";
+
         }else {
+          // Show that the user MUST select a position
           positionButtons.forEach(button => {
             button.classList.add("error")
             setTimeout(function() {
